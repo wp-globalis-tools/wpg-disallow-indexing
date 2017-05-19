94c87d11b3d0c695bf74a4355133bde3cdb77410
Pierre Dargham <pierre.dargham@globalis-ms.com>
2017-05-19 10:59:20 +0200

    Add robots.txt disallow all

--- wpg-disallow-indexing.php
+++ wpg-disallow-indexing.php
@@ -3,15 +3,22 @@
 Plugin Name:  Disallow Indexing
 Plugin URI:   https://github.com/wp-globalis-tools/wpg-disallow-indexing
 Description:  Disallow indexing of your site on non-production environments
-Version:      0.3.0
+Version:      0.4.0
 Author:       Pierre Dargham, GLOBALIS media system
 Author URI:   https://github.com/wp-globalis-tools/wpg-disallow-indexing
 */
 
+namespace Globalis\DisallowIndexing;
+
+
 if (WP_ENV !== 'production') {
-	if(is_admin()) {
-		define('WPG_NOINDEX', true);
-	} else {
-		add_filter('pre_option_blog_public', '__return_zero');
-	}
+	define('WPG_NOINDEX', true);
+	add_filter('pre_option_blog_public', '__return_zero');
+	add_filter('robots_txt', __NAMESPACE__ . '\\robots_txt', 99, 2);
+}
+
+function robots_txt($output, $public) {
+	$output = "User-agent: *\r\n";
+	$output .= "Disallow: /\r\n";
+	return $output;
 }
